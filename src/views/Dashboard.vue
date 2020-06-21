a5ee8a60db4aca26cb35008c4ea0c71e6720227d
chaseBrow <46569903+chaseBrow@users.noreply.github.com>
2020-06-21 17:17:01 -0400

    learning
    done

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,28 +7,31 @@
         <v-toolbar flat dark>
           <v-toolbar-title>Today's Summary</v-toolbar-title>
         </v-toolbar>
-        <v-list subheader two-line shaped elevation="24" style="max-height: 750px" class="overflow-y-auto">
-          <v-subheader>Upcoming</v-subheader>
-          <v-list-item class="listItem" v-for="engagement in engagements" :key="engagement.engagement" @click="test = !test">
-            <v-list-item-content>
-              <v-list-item-title class="font-weight-bold title">You have a {{engagement.engagement}} with {{engagement.who}}</v-list-item-title>
-              <v-list-item-subtitle class="font-weight-bold">@{{engagement.when}}</v-list-item-subtitle>
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-icon>mdi-arrow-right</v-icon>
-            </v-list-item-action>
-          </v-list-item>
-          <v-subheader>Past</v-subheader>
-          <v-list-item class="listItem" v-for="engagement in engagements" :key="engagement.engagement" @click="test = !test">
-            <v-list-item-content>
-              <v-list-item-title class="font-weight-bold title">You have a {{engagement.engagement}} with {{engagement.who}}</v-list-item-title>
-              <v-list-item-subtitle class="font-weight-bold">@{{engagement.when}}</v-list-item-subtitle>
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-icon>mdi-arrow-right</v-icon>
-            </v-list-item-action>
-          </v-list-item>
-        </v-list>
+
+        <v-card
+
+<!--        <v-list subheader two-line shaped elevation="24" style="max-height: 750px" class="overflow-y-auto">-->
+<!--          <v-subheader>Upcoming</v-subheader>-->
+<!--          <v-list-item class="listItem" v-for="engagement in engagements" :key="engagement.engagement" @click="test = !test">-->
+<!--            <v-list-item-content>-->
+<!--              <v-list-item-title class="font-weight-bold title">You have a {{engagement.engagement}} with {{engagement.who}}</v-list-item-title>-->
+<!--              <v-list-item-subtitle class="font-weight-bold">@{{engagement.when}}</v-list-item-subtitle>-->
+<!--            </v-list-item-content>-->
+<!--            <v-list-item-action>-->
+<!--              <v-icon>mdi-arrow-right</v-icon>-->
+<!--            </v-list-item-action>-->
+<!--          </v-list-item>-->
+<!--          <v-subheader>Past</v-subheader>-->
+<!--          <v-list-item class="listItem" v-for="engagement in engagements" :key="engagement.engagement" @click="test = !test">-->
+<!--            <v-list-item-content>-->
+<!--              <v-list-item-title class="font-weight-bold title">You have a {{engagement.engagement}} with {{engagement.who}}</v-list-item-title>-->
+<!--              <v-list-item-subtitle class="font-weight-bold">@{{engagement.when}}</v-list-item-subtitle>-->
+<!--            </v-list-item-content>-->
+<!--            <v-list-item-action>-->
+<!--              <v-icon>mdi-arrow-right</v-icon>-->
+<!--            </v-list-item-action>-->
+<!--          </v-list-item>-->
+<!--        </v-list>-->
       </v-col>
 
       <!--This is the Approve Engagement list      -->
