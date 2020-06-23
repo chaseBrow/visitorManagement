d16c2b0df7a8b3bb8dc96c13f46083f5c9fb7673
bjharrington <brandon.harrington@betheluniversity.edu>
2020-06-23 13:12:59 -0400

    Search Bars

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,26 @@
 <template>
 	<v-container fluid fill-height background>
 		<v-row class="mx-4" style="height: 100%">
-			<v-col cols="12" class="primary" style="border-radius: 10px 10px 0px 0px; height: auto">
-				<v-form>
-					<!-- <v-text-field label="First Name" outlined color="black"></v-text-field> -->
-				</v-form>
-			</v-col>
+			<v-form style="border-radius: 10px 10px 0px 0px; height: auto; width: 100%" class="primary">
+				<v-container>	
+					<v-row>
+						<v-col cols="2">
+							<v-text-field  label="First Name" outlined color="black"></v-text-field>
+						</v-col>
+						<v-col cols="2">
+							<v-text-field  label="Last Name" outlined color="black"></v-text-field>
+						</v-col>
+						<v-col cols="2">
+							<v-text-field  label="Company" outlined color="black"></v-text-field>
+						</v-col>
+						<v-col cols="3">
+							<v-text-field label="Email" outlined color="black"></v-text-field>
+						</v-col>
+					</v-row>
+				</v-container>
+			</v-form>
 			<v-col cols="12" class="secondary" style="border-radius: 0px 0px 10px 10px; height: auto">
-
+				
 			</v-col>	
 		</v-row>
 	</v-container>
