0c95e4b0d0e9b2211eef0617512f6316a66cca70
chaseBrow <46569903+chaseBrow@users.noreply.github.com>
2020-06-23 23:39:38 -0400

    learning

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,15 +2,15 @@
 	<v-container fluid fill-height background>
 		<v-row class="mx-4" style="height: 100%">
 			<v-form style="border-radius: 10px 10px 0px 0px; height: auto; width: 100%" class="primary">
-				<v-container>	
-					<v-row>
+				<v-container class="align-start">	
+					<v-row class="align-start">
 						<v-col cols="2">
 							<v-text-field  label="First Name" outlined color="black"></v-text-field>
 						</v-col>
 						<v-col cols="2">
 							<v-text-field  label="Last Name" outlined color="black"></v-text-field>
 						</v-col>
-						<v-col cols="2">
+						<v-col cols="3">
 							<v-text-field  label="Company" outlined color="black"></v-text-field>
 						</v-col>
 						<v-col cols="3">
@@ -19,12 +19,10 @@
 						<v-col cols="2">
 							<v-select label="Access" outlined color="black" :items="options"></v-select>
 						</v-col>
-						<v-col cols="1" class="">
-							<v-btn x-large width="100%">
-								<!-- <v-col cols="12">Today's</v-col>
-								<v-col cols="12">Visitors</v-col> -->
-								<span>Today's Visitor</span>
-								<v-icon>mdi-magnify</v-icon>
+						<v-col cols="2">
+							<v-btn x-large>
+								<span> Today's Visitor </span>
+								<v-icon class="pl-2">mdi-magnify</v-icon>
 							</v-btn>
 						</v-col>
 							
