00e5c3e4ac92027ebfe877541c74688cdffc23a0
Chase Brown <rappybrown@gmail.com>
2020-07-11 22:03:21 -0400

    nav broke

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,19 +8,20 @@
 		<v-spacer></v-spacer>
 		<VisitorInfo>
 		</VisitorInfo>
-		<v-btn class="primary" v-on:click="router">
+		<v-btn class="primary">
 			<span class="mr-1 black--text">Sign Out</span>
 			<v-icon color="black">mdi-exit-to-app</v-icon>
 		</v-btn>
 
-		<v-navigation-drawer  v-model="drawer" class="secondary" temporary app>
+		<v-navigation-drawer  v-model="drawer" class="secondary" app>
+			
 			<v-list>
-				<v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+				<v-list-item v-for="link in links" :key="link.text" :to="link.route" >
 					<v-list-item-action>
 						<v-icon class="white--text">{{ link.icon }}</v-icon>
 					</v-list-item-action>
 					<v-list-item-content>
-						<v-list-item-title class="white--text">
+						<v-list-item-title class="white--text" v-on:click="console.log('test')">
 							{{ link.text }}
 						</v-list-item-title>
 					</v-list-item-content>
@@ -48,9 +49,6 @@ export default {
 		}
 	},
 	methods: {
-		router: function () {
-			console.log(this.$route);
-		},
 	}
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,34 @@
 <template>
     <v-container>
-        <span> HELLLOOOO sgdasbfahsbfksbjfb Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque culpa earum excepturi, ex consequuntur voluptatem dolores quo corrupti. Cupiditate rem quo ratione provident sint dignissimos quaerat, illo quis magni fugiat?
-        </span>
+        <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>
+                    Login form
+                </v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
     </v-container>
 </template>
 <script>
