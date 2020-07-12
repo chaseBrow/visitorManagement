068ffc74683b739cc26c879891fb2a53764f7f4b
Chase Brown <rappybrown@gmail.com>
2020-07-11 21:37:28 -0400

    check login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
-    <v-card>
-        Hello Home Page
-    </v-card>
+    <v-container>
+        <span> HELLLOOOO sgdasbfahsbfksbjfb Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque culpa earum excepturi, ex consequuntur voluptatem dolores quo corrupti. Cupiditate rem quo ratione provident sint dignissimos quaerat, illo quis magni fugiat?
+        </span>
+    </v-container>
 </template>
 <script>
 export default {
