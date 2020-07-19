47647959c52f93285628c1cbd45067c1cffeb413
Chase Brown <rappybrown@gmail.com>
2020-07-18 22:55:37 -0400

    nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,7 @@
 			<v-icon color="black">mdi-exit-to-app</v-icon>
 		</v-btn>
 		<v-btn class="primary" v-if="['home'].indexOf($route.name) == 0">
-			<span class="mr-1 black--text">Sign In</span>
+			<span class="mr-1 black--text">Login</span>
 			<v-icon color="black">mdi-exit-to-app</v-icon>
 		</v-btn>
 
