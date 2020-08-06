6cc276d89396bb9a6244fe520c65e86f6fd7765d
ChadBrow <68403385+ChadBrow@users.noreply.github.com>
2020-08-06 19:11:22 -0400

    Started work on the new page

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,14 +1,40 @@
 <template>
   	<v-container>
-    	<Print> </Print>
+    	<v-row>
+			<v-col cols="8" class="blue" >
+				<v-row>
+					<v-col cols="8">
+						<span style="font-size: 32px; font-weight: bold;">Account Information</span>
+					</v-col>
+					<v-col cols="4">
+						<v-btn> Edit </v-btn>
+					</v-col>
+				</v-row>
+				<v-row>
+					<v-col cols="8">
+						<v-text-field label="Username" solo flat>
+						</v-text-field>
+						<v-text-field label="Password">
+						</v-text-field>
+						<v-text-field label="Email">
+						</v-text-field>
+					</v-col>
+				</v-row>
+			</v-col>
+			<v-col cols="4" class="red">
+			</v-col>
+    	</v-row>
+		<v-row>
+			<v-col cols="12" class="green">
+			</v-col>
+		</v-row>
   	</v-container>
 </template>
 
 <script>
-import Print from '../components/Print'
   	export default {
   		components: {
-			  Print
+
 		},
 		methods: {
 
