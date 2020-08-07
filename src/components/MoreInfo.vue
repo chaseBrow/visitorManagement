a7892fce2a8a9c0e39b6dd12adda0d08808fbf11
Chase Brown <rappybrown@gmail.com>
2020-08-06 23:23:59 -0400

    ids

--- src/components/MoreInfo.vue
+++ src/components/MoreInfo.vue
@@ -2,7 +2,7 @@
     <v-container class="ma-0 pa-0">
         <v-dialog v-model="dialog" width="80%" class="primary">
             <template v-slot:activator="{ on, attrs }">
-                <v-btn v-bind="attrs" v-on="on" class="primary" v-on:click="getInfo()">
+                <v-btn v-bind="attrs" v-on="on" class="primary">
                     <v-icon dense class="pr-1">mdi-card-account-details-outline</v-icon>
                     <span>Info</span>
                 </v-btn>
@@ -14,49 +14,51 @@
                     </v-card-title>
                     <v-row>
                         <v-col cols="4">
-                            <v-text-field rounded readonly v-model="user.firstName" label="First Name">
+                            <v-text-field rounded readonly v-model="user.firstName" label="First Name" :id="user.firstName">
                             </v-text-field>
                             <!-- remove rounded and readonly add outlined -->
                         </v-col>
                         <v-col cols="4">
-                            <v-text-field rounded readonly v-model="user.lastName" label="Last Name">
+                            <v-text-field rounded readonly v-model="user.lastName" label="Last Name" :id="user.lastName">
                             </v-text-field>
                         </v-col>
                         <v-col cols="4">
-                            <v-text-field rounded readonly v-model="user.company" label="Company">
+                            <v-text-field rounded readonly v-model="user.company" label="Company" :id="user.company + 'text'">
                             </v-text-field>
                             <v-autocomplete outlined label="Company" color="black" cache-items hide-no-data
 								:items="companyFinal"
       							:search-input.sync="searchComp"
 								v-model="user.company"
                                 style="visibility: hidden; position: fixed"
+                                :id="user.company"
 							>
 							</v-autocomplete>
                         </v-col>
                     </v-row>
                     <v-row>
                         <v-col cols="4">
-                            <v-text-field rounded readonly label="Email" v-model="user.email">
+                            <v-text-field rounded readonly label="Email" v-model="user.email" :id="user.email">
                             </v-text-field>
                         </v-col>
                         <v-col cols="4">
-                            <v-text-field rounded readonly v-model="user.lastName" label="Access">
+                            <v-text-field rounded readonly v-model="user.access" label="Access" :id="user.access + 'text'">
                             </v-text-field>
                             <v-select outlined label="Access" :items="options" v-model="user.access" 
                                 v-on:focus="getOptions"
                                 style="visibility: hidden; position: fixed"
+                                :id="user.access"
                             >
                             </v-select>
                         </v-col>
                         <v-col cols="4">
-                            <v-text-field rounded readonly label="Phone" v-model="user.phone">
+                            <v-text-field rounded readonly label="Phone" v-model="user.phone" :id="user.phone">
                             </v-text-field>
                         </v-col>
                     </v-row>
-                    <v-row class="d-flex align-start justify-center ">
-                        <v-checkbox class="red" label="May Schedule Others">
+                    <v-row class="d-flex justify-space-around">
+                        <v-checkbox readonly v-model="user.maySchedule" label="May Schedule Others">
                         </v-checkbox>
-                        <v-checkbox class="blue" label="May Request Remote Hands">
+                        <v-checkbox readonly v-model="user.mayRequest" label="May Request Remote Hands">
                         </v-checkbox>
                     </v-row>
                 </v-card>
@@ -76,10 +78,12 @@ export default {
             user: {
                 firstName: null,
                 lastName: null,
-                company: "test",
+                company: null,
                 email: null,
                 access: null,
                 phone: null,
+                maySchedule: null,
+                mayRequest: null,
             },
             options: [],
             access: null,
@@ -94,11 +98,12 @@ export default {
         let comp = this.person.get('company');
         this.user.firstName = this.person.get('firstName');
         this.user.lastName = this.person.get('lastName');
-        //this.user.company = comp.get('name');
+        this.user.company = comp.get('name');
         this.user.email = this.person.get('email');
         this.user.access = this.person.get('access');
         this.user.phone = this.person.get('phone');
-        console.log(comp)
+        this.user.maySchedule = this.person.get('maySchedule');
+        this.user.mayRequest = this.person.get('mayRequest');
     },
     methods: {
         getOptions: function () {
@@ -124,21 +129,20 @@ export default {
 			test.forEach( e =>{
 				this.companyFinal.push(e.get("name"));
 			})
-		},
-        getInfo: async function () {
-            this.dialog = true;
-            let fields = [];
-            fields = await document.getElementsByClassName('visitor');
-            
-            for (let field of fields) {
-                console.log(field);
-            }
+        },
+        save: function () {
+
         },
         cancel: function () {
             console.log(this.dialog);
             this.dialog = false;
-        }
+        },
+        delete: function () {
 
+        },
+        edit: function () {
+            
+        },
     }
     
 }
