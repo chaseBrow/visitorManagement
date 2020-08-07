e6a38f51ef1ef0450388b70d61970c7c61068516
ChadBrow <68403385+ChadBrow@users.noreply.github.com>
2020-08-07 18:11:26 -0400

    Update Account.vue

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -12,7 +12,7 @@
 						</v-btn>
 					</v-col>
 					<v-col cols="8">
-						<v-text-field label="Username" rounded reasdonly background-color="secondary" v-model="user.username" style="font-size: 18px"> 
+						<v-text-field label="Username" rounded readonly background-color="secondary" v-model="user.username" style="font-size: 18px"> 
 							<!-- add outlined and remove readonly -->
 						</v-text-field>
 					</v-col>
@@ -24,7 +24,7 @@
 						</v-btn>
 					</v-col>
 					<v-col cols="8">
-						<v-text-field label="Email" rounded reasdonly background-color="secondary" v-model="user.email" style="font-size: 18px"> 
+						<v-text-field label="Email" rounded readonly background-color="secondary" v-model="user.email" style="font-size: 18px"> 
 							<!-- add outlined and remove readonly -->
 						</v-text-field>
 					</v-col>
