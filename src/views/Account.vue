4ef5dc0bb39b387eea5631570f5045a54f560fec
ChadBrow <68403385+ChadBrow@users.noreply.github.com>
2020-08-06 20:03:38 -0400

    Finnished formatting account information

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -3,23 +3,36 @@
     	<v-row>
 			<v-col cols="8" class="secondary" >
 				<v-row>
-					<v-col cols="9">
-						<span style="font-size: 32px; font-weight: bold;">Account Information</span>
+					<span style="padding: 0px 0px 0px 20px; font-size: 32px; font-weight: bold;">Account Information</span>
+				</v-row>
+				<v-row>
+					<v-col cols="2" class="d-flex justify-end px-0">
+						<v-btn> 	
+							Edit
+						</v-btn>
 					</v-col>
-					<v-col cols="3" class="d-flex align-center justify-center" >
-						<v-btn style="font-size: 18px;"> Edit Profile</v-btn>
+					<v-col cols="8">
+						<v-text-field label="Username" rounded reasdonly background-color="secondary" v-model="user.username" style="font-size: 18px"> 
+							<!-- add outlined and remove readonly -->
+						</v-text-field>
 					</v-col>
 				</v-row>
 				<v-row>
+					<v-col cols="2" class="d-flex justify-end px-0">
+						<v-btn> 	
+							Edit
+						</v-btn>
+					</v-col>
 					<v-col cols="8">
-						<v-text-field label="Username" rounded readonly background-color="secondary" v-model="user.username" style="font-size: 18px">
-						</v-text-field>
-						<v-text-field label="Password" rounded readonly background-color="secondary" v-model="user.password" style="font-size: 18px">
-						</v-text-field>
 						<v-text-field label="Email" rounded reasdonly background-color="secondary" v-model="user.email" style="font-size: 18px"> 
 							<!-- add outlined and remove readonly -->
 						</v-text-field>
 					</v-col>
+				</v-row>
+				<v-row class="ml-6">
+					<v-btn>
+						Reset Password
+					</v-btn>
 				</v-row>
 			</v-col>
 			<v-col cols="4" class="red">
@@ -39,20 +52,8 @@ import Parse from 'parse'
 			return {
 				user: {
 					username: "testUser",
-					passwordHid: this.passwordHide,
-					password: null,
 					email: null,
 				},
-			}
-		},
-		computed: {
-			passwordHide: function () {
-				let pass = "";
-				for (let i = 1; i <= this.password.length; i += 1) {
-					pass += "*";
-				}
-				console.log("hi")
-				return pass;
 			}
 		},
   		components: {
@@ -65,8 +66,6 @@ import Parse from 'parse'
 			getUser: function () {
 				let User = Parse.User.current();
 				this.user.usernmae = User.get('username');
-				this.user.password = User.get('password');
-				console.log(User.get('password'));
 				this.user.email = User.get('email');
 			}
 
